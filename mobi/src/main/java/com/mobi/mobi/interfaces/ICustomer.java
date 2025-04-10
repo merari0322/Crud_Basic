8400bcfdf45b62728f1f82e96888e3d89d341154
merari <merariurbano0322@gmail.com>
2025-04-10 09:08:45 -0500

    a

---
+++ mobi/src/main/java/com/mobi/mobi/interfaces/ICustomer.java
@@ -0,0 +1,13 @@
+package com.mobi.mobi.interfaces;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface ICustomer {
+        exte JpaRepository<Customer,Integer>{
+
+        }
+
+}
